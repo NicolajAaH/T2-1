31ae1d536420415227ba8a433b62d943a61145f5
Nicolaj Hansen <nicolaj.it@icloud.com>
2020-11-03 13:49:01 +0100

    tilføjet scanner og ændret ting til dansk

--- zool/src/worldofzuul/Player.java
+++ zool/src/worldofzuul/Player.java
@@ -2,7 +2,7 @@ package worldofzuul;
 
 public class Player {
     String name;
-    private int wallet = 30000;
+    private int wallet = 0;
     int score;
     private Inventory inventory = new Inventory();
 
@@ -16,6 +16,10 @@ public class Player {
 
     public Inventory getInventory() {
         return inventory;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
     }
 }
 
