f6318d4b9fd8cc6ad31bca672fd341aae7b0a149
Theis Langlands <theislanglands@gmail.com>
2020-12-01 09:33:52 +0100

    Domain connect

--- zool/src/Energy/domain/DomainConnect.java
+++ zool/src/Energy/domain/DomainConnect.java
@@ -1,2 +1,20 @@
-package Energy.domain;public class DomainConnect {
+package Energy.domain;
+
+import Energy.Interface.DomainI;
+
+public class DomainConnect implements DomainI {
+
+    private Game game;
+
+    public DomainConnect(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public Inventory getPlayerInventory() {
+        return game.getPlayer().getInventory();
+    }
+
+    
+
 }
