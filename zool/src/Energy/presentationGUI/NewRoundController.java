60802bcdc5a23a65bf78ff69c2d02bfead879245
Nicolaj Hansen <nicolaj.it@icloud.com>
2020-12-04 11:38:38 +0100

    Næsten færdig

---
+++ zool/src/Energy/presentationGUI/NewRoundController.java
@@ -0,0 +1,19 @@
+package Energy.presentationGUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class NewRoundController extends Controller{
+
+    @FXML
+    private Label textNewYear;
+    @FXML
+    private Button exitGame;
+    @FXML
+    private Button newYear;
+
+    public void initialize(){
+
+    }
+}
---
+++ zool/src/Energy/presentationGUI/Start.java
@@ -0,0 +1,55 @@
+package Energy.presentationGUI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class Start extends Controller{
+    @FXML
+    private Button okButton;
+    @FXML
+    private TextField input;
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Label errorLabel;
+
+    public void initialize(){
+
+        welcomeText.setText(getDomainI().welcomeText());
+
+        okButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                while(true) {
+                    String error = getDomainI().setStartAmountGUI(input.getText());
+                    errorLabel.setText(error);
+                    if(error == null){
+                        try {
+                            runGui.getFxmlLoader().load(getClass().getResource("Outside.fxml"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Parent newRoot = null;
+                        try {
+                            newRoot = runGui.getFxmlLoader().load(getClass().getResource("Outside.fxml"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        runGui.getStage().setScene(new Scene(newRoot));
+                        runGui.getStage().show();
+                        break;
+                    }
+                    break;
+                }
+            }
+        });
+    }
+}
